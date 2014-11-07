39c3c8bd244cb9eeb46a4fe78fd1da0a68ac0c36
Nils Winkler <nils@nilswinkler.com>
2014-11-07 12:11:10 +0100

    Added more initial files.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            files: [
+                'Gruntfile.js',
+                'bin/*',
+                'lib/**/*.js',
+                'test/**/*.js'
+            ]
+        },
+    });
+
+    grunt.registerTask('default', 'jshint');
+};
