665a41b1ccb2e5a5bd8f893c59be6b43aea731c0
Nils Winkler <nils@nilswinkler.com>
2014-11-10 17:37:27 +0100

    Added more tests for mocking the available call.

--- test/commands/available.js
+++ test/commands/available.js
@@ -4,12 +4,42 @@ var mockSpawn = require('mock-spawn');
 var mySpawn = mockSpawn();
 require('child_process').spawn = mySpawn;
 
-describe.only('available', function () {
+describe('available', function () {
     var available = require('../../lib/commands/available');
 
     it('should return false when the helper is not available', function (done) {
         available(function(err, isAvailable) {
             expect(isAvailable).to.be(false);
+
+            done();
+        });
+    });
+
+    it('should return false when the helper is not available and the git command exits with an error', function (done) {
+        mySpawn.sequence.add(mySpawn.simple(1));
+
+        available(function(err, isAvailable) {
+            expect(isAvailable).to.be(false);
+
+            done();
+        });
+    });
+
+    it('should return false when the helper is not available and the git command exits with an error and error message', function (done) {
+        mySpawn.sequence.add(mySpawn.simple(1, null, 'git error'));
+
+        available(function(err, isAvailable) {
+            expect(isAvailable).to.be(false);
+
+            done();
+        });
+    });
+
+    it('should return true when the helper is available', function (done) {
+        mySpawn.sequence.add(mySpawn.simple(0, 'osxkeychain'));
+
+        available(function(err, isAvailable) {
+            expect(isAvailable).to.be(true);
 
             done();
         });
