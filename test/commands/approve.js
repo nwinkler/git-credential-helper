d0e9213173a339fedd0a6790f2480a78777db1e8
Nils Winkler <nils@nilswinkler.com>
2014-11-13 07:39:30 +0100

    Added mock tests for approve and reject commands.

--- test/commands/approve.js
+++ test/commands/approve.js
@@ -1,3 +1,60 @@
+var expect = require('expect.js');
+var sinon = require('sinon');
+var mockery = require('mockery');
 
 describe('approve', function () {
+    var executeCredential;
+    var repo;
+    var libPath = '../../lib/commands/approve';
+    var approve;
+
+    beforeEach(function () {
+        executeCredential = sinon.stub();
+        repo = sinon.stub();
+
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+
+        mockery.registerMock('../util/executeCredential', executeCredential);
+        mockery.registerMock('../util/repo', repo);
+        mockery.registerAllowable(libPath, true);
+        approve = require(libPath);
+    });
+
+    afterEach(function() {
+        mockery.deregisterAll();
+        mockery.resetCache();
+        mockery.disable();
+    });
+
+    it('should return nothing when the tool has accepted the approval', function (done) {
+        var testServer = { mock: 'foo'};
+        var testTarget = 'http://foo.not.found';
+        var credentials = {
+            username: 'user',
+            password: 'pass'
+        };
+        var testServerCredentials = {
+            mock: 'foo',
+            username: 'user',
+            password: 'pass'
+        };
+        executeCredential.yields(null, {});
+        repo.returns(testServer);
+
+        approve(testTarget, credentials, function(err, data) {
+            expect(data).to.eql({});
+
+            done();
+        });
+
+        expect(executeCredential.called).to.be(true);
+        expect(executeCredential.args[0][0]).to.eql(['approve']);
+        expect(executeCredential.args[0][2]).to.eql(testServerCredentials);
+
+        expect(repo.called).to.be(true);
+        expect(repo.args[0][0]).to.be(testTarget);
+    });
 });
--- test/commands/reject.js
+++ test/commands/reject.js
@@ -1,3 +1,51 @@
+var expect = require('expect.js');
+var sinon = require('sinon');
+var mockery = require('mockery');
 
 describe('reject', function () {
+    var executeCredential;
+    var repo;
+    var libPath = '../../lib/commands/reject';
+    var reject;
+
+    beforeEach(function () {
+        executeCredential = sinon.stub();
+        repo = sinon.stub();
+
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+
+        mockery.registerMock('../util/executeCredential', executeCredential);
+        mockery.registerMock('../util/repo', repo);
+        mockery.registerAllowable(libPath, true);
+        reject = require(libPath);
+    });
+
+    afterEach(function() {
+        mockery.deregisterAll();
+        mockery.resetCache();
+        mockery.disable();
+    });
+
+    it('should return nothing when the tool has accepted the rejection', function (done) {
+        var testServer = { mock: 'foo'};
+        var testTarget = 'http://foo.not.found';
+        executeCredential.yields(null, {});
+        repo.returns(testServer);
+
+        reject(testTarget, function(err, data) {
+            expect(data).to.eql({});
+
+            done();
+        });
+
+        expect(executeCredential.called).to.be(true);
+        expect(executeCredential.args[0][0]).to.eql(['reject']);
+        expect(executeCredential.args[0][2]).to.eql(testServer);
+
+        expect(repo.called).to.be(true);
+        expect(repo.args[0][0]).to.be(testTarget);
+    });
 });
