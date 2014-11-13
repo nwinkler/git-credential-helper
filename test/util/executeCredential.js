d45b077cedc67d79f16f95a98a50672707eb2cf7
Nils Winkler <nils@nilswinkler.com>
2014-11-13 08:39:12 +0100

    Stubbing out feed functionality.

--- test/util/executeCredential.js
+++ test/util/executeCredential.js
@@ -1,20 +1,26 @@
 var expect = require('expect.js');
 var mockSpawn = require('mock-spawn');
+var sinon = require('sinon');
+
 var mockery = require('mockery');
 
 describe('executeCredential', function () {
     var mySpawn;
+    var feed;
 
     var executeCredential;
     var libPath = '../../lib/util/executeCredential';
 
     beforeEach(function () {
         mySpawn = mockSpawn(false);
+        feed = sinon.stub();
+
         mockery.enable({
             useCleanCache: true,
             warnOnUnregistered: false
         });
         mockery.registerMock('child_process', { spawn: mySpawn });
+        mockery.registerMock('../util/feed', feed);
         mockery.registerAllowable(libPath, true);
         executeCredential = require(libPath);
     });
@@ -38,5 +44,26 @@ describe('executeCredential', function () {
 
             done();
         }, '');
+    });
+
+    it('should call git credential when called with input data', function (done) {
+        mySpawn.sequence.add(mySpawn.simple(0, '\n'));
+
+        // TODO Verify feed call
+        executeCredential(['bar1', 'bar2'], function(err, data) {
+            expect(err).to.be(null);
+            expect(data).to.eql({});
+
+            var call = mySpawn.calls[0];
+            expect(call.command).to.be('git');
+            expect(call.args).to.eql(['credential', 'bar1', 'bar2']);
+
+            done();
+        }, {
+            foo: 'bar',
+            foo2: 'bar2',
+            foo3: 'bar3',
+            foo4: ''
+        });
     });
 });
